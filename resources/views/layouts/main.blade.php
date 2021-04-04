74a4ec8df38592380da50a4ccf53197b5f0f4352
Ted <p0599847@protonmail.com>
2021-04-04 15:31:41 +0200

    Update main.blade.php

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -13,31 +13,31 @@
 
 <body class="font-sans bg-gray-900 text-white">
     <nav class="border-b border-gray-800 px-4">
-        <div class="container p-4 mx-auto flex items-center justify-between px-4 py-6">
-            <ul class="flex items-center">
+        <div class="container p-4 mx-auto flex flex-col md:flex-row items-center justify-between px-4 py-6">
+            <ul class="flex flex-col md:flex-row items-center ">
                 <li>
                     <a href="#">
                         <img src="{{ asset('images/800px-Netflix_2015_N_logo.svg.png') }}" alt="app" class="w-5">
                     </a>
                 </li>
-                <li class="ml-16">
+                <li class="md:ml-16 mt-3 md:mt-0">
                     <a href="" class="hover::text-gray-300">Movies</a>
                 </li>
-                <li class="ml-8">
+                <li class="md:ml-6 mt-3 md:mt-0">
                     <a href="" class="hover::text-gray-300">TV Shows</a>
                 </li>
-                <li class="ml-8">
+                <li class="md:ml-6 mt-3 md:mt-0">
                     <a href="" class="hover::text-gray-300">Actors</a>
                 </li>
             </ul>
-            <div class="flex items-center">
-                <div class="relative">
+            <div class="flex flex-col md:flex-row items-center">
+                <div class="relative mt-3 md:mt-0">
                     <input type="text" class="bg-gray-800 text-sm rounded-full w-64 px-4 pl-8 py-1 focus:outline-none focus:shadow-outline" placeholder="Search">
                     <div class="absolute top-0">
                         <i class="fas fa-search w-4 mt-2 ml-2 fill-current text-gray-500"></i>
                     </div>
                 </div>
-                <div class="ml-4">
+                <div class="md:ml-4 mt-3 md:mt-0">
                     <a href="#">
                         <img src="{{ asset('images/DSC_0676-min.JPG') }}" alt="avatar" class="rounded-full w-8 h-8">
                     </a>
