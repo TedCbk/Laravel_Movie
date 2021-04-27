f017c6fc7dfd10100debb16dec5a4cbcbc10595f
Ted <p0599847@protonmail.com>
2021-04-27 12:23:21 +0200

    Update search dropdown and movie trailer

--- resources/views/livewire/search-dropdown.blade.php
+++ resources/views/livewire/search-dropdown.blade.php
@@ -3,6 +3,13 @@
         <input wire:model.debounce.500ms="search" type="text"
             class="bg-gray-800 text-sm rounded-full w-64 px-4 pl-8 py-1 focus:outline-none focus:shadow-outline"
             placeholder="Search"
+            x-ref="search"
+            @keydown.window="
+                if(event.keyCode === 191){
+                    event.preventDefault();
+                    $refs.search.focus();
+                }
+            "
             @focus="isOpen = true"
             @keydown="isOpen = true"
             @keydown.escape.window="isOpen = false"
